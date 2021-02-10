6fc2de07e2971fe5eed50730145ca432b77ae80c
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-02-10 17:11:00 +0100

    Change pointEquals to arrow style

--- src/js/snake.js
+++ src/js/snake.js
@@ -9,6 +9,4 @@ Point = function(x, y) {
     this.y = y;
 };
 
-pointEquals = function(p1, p2) {
-    return p1.x == p2.x && p1.y == p2.y;
-};
+pointEquals = p1 => p2 => p1.x == p2.x && p1.y == p2.y;
