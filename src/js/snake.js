bf8a15d9dd0737ad63ab01e9ca0d15762fe0973d
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-02-10 18:33:32 +0100

    Add directions, remove TILE_SIZE

--- src/js/snake.js
+++ src/js/snake.js
@@ -1,10 +1,14 @@
 "use strict";
 
-const TILE_SIZE = 16;
-const WIDTH = 256 / TILE_SIZE;
-const HEIGHT = 160 / TILE_SIZE;
+const WIDTH = 16;
+const HEIGHT = 10;
 
 const Point = (x, y) => ({x: x, y: y});
+
+const UP    = Point(0, -1)
+const DOWN  = Point(0, 1)
+const LEFT  = Point(-1, 0)
+const RIGHT = Point(1, 0)
 
 const pointEquals = (p1, p2) => (p1.x === p2.x) && (p1.y === p2.y);
 
