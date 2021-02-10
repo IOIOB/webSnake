13bf8b48d95bf150d2b8c698c7df2e74c35cfc49
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-02-10 17:22:08 +0100

    Change == to === and add space after 'function'

--- src/js/snake.js
+++ src/js/snake.js
@@ -4,9 +4,9 @@ const TILE_SIZE = 16;
 const WIDTH = 256 / TILE_SIZE;
 const HEIGHT = 160 / TILE_SIZE;
 
-const Point = function(x, y) {
+const Point = function (x, y) {
     this.x = x;
     this.y = y;
 };
 
-const pointEquals = (p1, p2) => p1.x == p2.x && p1.y == p2.y;
+const pointEquals = (p1, p2) => p1.x === p2.x && p1.y === p2.y;
