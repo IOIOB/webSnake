26cac25bdbe6ceed01dea05b691d00d5c91f49a8
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-02-10 17:19:02 +0100

    Added const to function declarations
    Yes this is p much my first time writing javascript lol

--- src/js/snake.js
+++ src/js/snake.js
@@ -1,12 +1,12 @@
-'use strict'
+"use strict";
 
 const TILE_SIZE = 16;
 const WIDTH = 256 / TILE_SIZE;
 const HEIGHT = 160 / TILE_SIZE;
 
-Point = function(x, y) {
+const Point = function(x, y) {
     this.x = x;
     this.y = y;
 };
 
-pointEquals = (p1, p2) => p1.x == p2.x && p1.y == p2.y;
+const pointEquals = (p1, p2) => p1.x == p2.x && p1.y == p2.y;
