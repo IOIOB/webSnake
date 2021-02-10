c970fef9a7f864ff8251b6614c45d74183069a93
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-02-10 18:16:01 +0100

    Add FG_COLOUR, change fill to drawBg

--- src/js/draw.js
+++ src/js/draw.js
@@ -1,16 +1,18 @@
 
-const BG_COLOUR = '#49494d';
+const BG_COLOUR = '#e6e3d9';
+const FG_COLOUR = '#49494d';
 const SNAKE_COLOUR = '#2db34c';
 const FOOD_COLOUR = '#bf413f';
 
 let canvas = document.getElementById('snake');
 let graphics = canvas.getContext('2d');
 
-let fill = function () {
+let drawBg = function () {
     graphics.fillStyle = BG_COLOUR;
     graphics.fillRect(0, 0, canvas.width, canvas.height);
+    graphics.fillStyle = FG_COLOUR
 };
 
 window.onload = function () {
-    fill();
+    drawBg();
 };
