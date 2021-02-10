8c0a2cd7cb8dbc0f17c9529f851d457cede714a0
IOIOB <70326335+IOIOB@users.noreply.github.com>
2021-02-10 18:16:47 +0100

    Change Point to arrow function

--- src/js/snake.js
+++ src/js/snake.js
@@ -4,9 +4,8 @@ const TILE_SIZE = 16;
 const WIDTH = 256 / TILE_SIZE;
 const HEIGHT = 160 / TILE_SIZE;
 
-const Point = function (x, y) {
-    this.x = x;
-    this.y = y;
-};
+const Point = (x, y) => ({x: x, y: y});
 
 const pointEquals = (p1, p2) => (p1.x === p2.x) && (p1.y === p2.y);
+
+let snake = [Point(5, 5)]
